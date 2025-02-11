b90b68a1c8ecf83099352010fb4facd9acc83383
Maxim Degtiarev <mxim.degt@gmail.com>
2025-02-11 19:53:28 +0000

    bug fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <section className='flex flex-col py-5 px-10 mx-10 items-center'>
       <div className='text-center mb-5'>
-        <h1 className='text-6xl'>Mara Flamini</h1>
-        <h2 className='text-3xl'>Consultorio dermatologico</h2>
+        <h1 className='text-4xl lg:text-6xl '>Mara Flamini</h1>
+        <h2 className='lg:text-3xl text-md'>Consultorio dermatologico</h2>
       </div>
       <AppointmentForm/>
     </section>
