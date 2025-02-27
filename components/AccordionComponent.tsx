5c53c44383f848e42aa4b4cc8dde31a75b4f55c9
Maxim Degtiarev <mxim.degt@gmail.com>
2025-02-27 17:28:16 -0300

    ...

--- components/AccordionComponent.tsx
+++ components/AccordionComponent.tsx
@@ -6,8 +6,6 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "./ui/select";
-import { Input } from "@/components/ui/input";
-
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 
